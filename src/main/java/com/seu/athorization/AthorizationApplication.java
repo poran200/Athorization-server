53223021b96bdbde5c7c2ceb8aacd626c2310998
Poran chowdury <chowdury659@gmail.com>
2019-09-27 17:44:50 +0600

    Initial commit

---
+++ src/main/java/com/seu/athorization/AthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.seu.athorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthorizationApplication.class, args);
+    }
+
+}
